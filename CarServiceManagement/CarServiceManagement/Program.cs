b76b43da3a78e162ae31a85d86a6bd6dcd44388f
Lucian Radu <radulucian.andrei@gmail.com>
2020-04-06 18:49:03 +0300

    Added base project

---
+++ CarServiceManagement/CarServiceManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarServiceManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

34723ee7ab958be0d66475759aff60e8b4a7a4f5
georgeandrei1998 <georgeandrei1998@gmail.com>
2020-04-24 14:24:59 +0300

    Added Command design pattern for printing the status of repair process.

--- CarServiceManagement/CarServiceManagement/State/NotStartedState.cs
+++ CarServiceManagement/CarServiceManagement/State/NotStartedState.cs
@@ -11,17 +11,17 @@ namespace CarServiceManagement.State
 
         public override void CheckCar()
         {
-            Console.WriteLine("Car is in service, not curently worked on");
+            CheckCarCommand.Execute("Car is in service, not curently worked on");
         }
 
         public override void CheckOutCar()
         {
-            Console.WriteLine("Car is not ready");
+            CheckOutCarCommand.Execute("Car is not ready");
         }
 
         public override void RepairCar()
         {
-            Console.WriteLine("Started working on car");
+            RepairCarCommand.Execute("Started working on car");
             Car.SetCarState(Car.WorkingOnState);
             Logger.Instance.LogOk("Car changed state");
         }
